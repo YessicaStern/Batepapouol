a70fc92bf5482e2744491aad6633750e58d68e22
YessicaStern <yessica_stern@hotmail.com>
2022-06-25 22:09:10 -0300

    consegui adicionar o post com sucesso mas antes foi necessário consertar alguns erros que tinha cometido e estava impossibilitando o axios.post

--- projeto5-batepapouol/script.js
+++ projeto5-batepapouol/script.js
@@ -8,37 +8,29 @@ let minutos = dataAtual.getMinutes();
 let segundos =dataAtual.getSeconds();
 
 
-verificarUsuario();
-function verificarUsuario(){
-   promessa=axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
-   promessa.then(executar);
-}
-function executar(resposta){
-   console.log(resposta);
-   usuario=resposta.data;
-}
  function entrar(el){ //VRIFICA SE OS USUÁRIOS SÃO DIFERENTES  \\falta terminar colocando um loop para caso de usuários com nome iguais
     nome = document.querySelector(".input-usuario").value;
+    nomeObj= {name: nome};
 
-    for(let i=0; i<usuario.length; i++){
-      if(nome===usuario[i].name){
-         alert("Esse nome já está em uso");
-         executar();
-      }else{   
-         el.parentNode.classList.add("esconder");
-         agoraPodeEntrar();
-         //verificaOnline();
-      }
-    }
+    let promessa= axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",nomeObj);
+    promessa.then(executar);
  }
+
+ function executar(resposta){
+    console.log(resposta);
+    usuario=resposta.data;
+    document.querySelector(".tela-inicial").classList.add("esconder");
+    agoraPodeEntrar();
+    setInterval(verificaOnline,4000);
+ }
+
 
 function verificaOnline(){
    let usuarioOnline ={ name: nome};
-   setInterval(function verificaOnline(){
-      let verifica = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", usuarioOnline);
-      console.log("MAE TA ONNNNNNNNNNNNNNN");
-      },4000);
+   axios.post("https://mock-api.driven.com.br/api/v6/uol/status", usuarioOnline);
+   console.log("MAE TA ONNNNNNNNNNNNNNN");
 }
+
 
 
 function entrou(){//ajeitar isso aqui colocar os stts e tudo
@@ -59,17 +51,14 @@ function buscarMensagens(){ //colocar um catch
    console.log("Mensagens Atualizadas")
 }
 
-
 function iniciarBatePapo(detalhes){
-  console.log(detalhes);
+  //console.log(detalhes);
   objDetalhes=detalhes.data;
   entrou();
   publicarMensagens();
 }
 
-
 function publicarMensagens(){
-   corpo.innerHTML="";
    for(let i=0; i<objDetalhes.length; i++){
       
       if(objDetalhes[i].type=="status"){
@@ -80,11 +69,10 @@ function publicarMensagens(){
         const mensagens= ` <div class="chat-publico mensagem"><h6>(${objDetalhes[i].time})</h6><h1>${objDetalhes[i].from}</h1><h2>para</h2><h1>${objDetalhes[i].to}</h1><h2>${objDetalhes[i].text}</h2></div>`
         corpo.innerHTML += mensagens;
   }}
-  const ultimaMensagem =document.querySelector(".mensagem:last-child");
-  ultimaMensagem.scrollIntoView();
-    //setInterval(buscarMensagens,3000);
+   const ultimaMensagem =document.querySelector(".mensagem:last-child");
+   ultimaMensagem.scrollIntoView();
+   setInterval(buscarMensagens,4500);
 }
-
 
 function novaMensagem(el){
    const novasMensagens = document.querySelector(".input-mensagem").value;
