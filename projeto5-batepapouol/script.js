46e0888740230ae257128eadaa07a5ddbb8cbd14
YessicaStern <yessica_stern@hotmail.com>
2022-06-27 14:07:07 -0300

    Nat me ajudou a ver onde estava o erro e acabou que eu estava num loop infinito num setInterval com o axios.get e acabou travando meu projeto quando ficava online mas após resolver isso deu tudo certo, fiz o bonus também mas ainda falta alguns detalhes do bonus

--- projeto5-batepapouol/script.js
+++ projeto5-batepapouol/script.js
@@ -1,34 +1,29 @@
 let nome;
-
+let contatos;
 let corpo = document.querySelector(".corpo");
 corpo.innerHTML = "";
 
-let dataAtual = new Date();
-let horas = dataAtual.getHours();
-horas=horas-9;
-let minutos = dataAtual.getMinutes();
-let segundos =dataAtual.getSeconds();
-
- function entrar(el){ //VRIFICA SE OS USUÁRIOS SÃO DIFERENTES  \\falta terminar colocando um loop para caso de usuários com nome iguais
+ function entrar(){ //VRIFICA SE OS USUÁRIOS SÃO DIFERENTES  \\falta terminar colocando um loop para caso de usuários com nome iguais
     nome = document.querySelector(".input-usuario").value;
     nomeObj= {name: nome};
-
     let promessa= axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",nomeObj);
     promessa.then(executar);
     promessa.catch(EscolherOutroNome);
  }
+
  function EscolherOutroNome(resposta){
-  // console.log(resposta);
    if(resposta.response.status===400){
    alert("Esse nome já está em uso por favor escolha outro");}
  }
 
  function executar(resposta){
-  //  console.log(resposta);
     usuario=resposta.data;
     document.querySelector(".tela-inicial").classList.add("esconder");
-    agoraPodeEntrar();
-    setInterval(verificaOnline,3000);
+    document.querySelector(".corpo").classList.remove("esconder");
+    document.querySelector(".topo").classList.remove("esconder");
+    document.querySelector(".bot").classList.remove("esconder");
+    setInterval(buscarMensagens,3000);
+    setInterval(verificaOnline,4000);
  }
 
 function verificaOnline(){
@@ -36,12 +31,6 @@ function verificaOnline(){
    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", usuarioOnline);
 }
 
-function agoraPodeEntrar(){    
-   document.querySelector(".corpo").classList.remove("esconder");
-   document.querySelector(".topo").classList.remove("esconder");
-   document.querySelector(".bot").classList.remove("esconder");
-   buscarMensagens();
-} 
 
 function buscarMensagens(){
    let carregarmensagens = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -50,46 +39,44 @@ function buscarMensagens(){
 
 function iniciarBatePapo(detalhes){ //IDENTIFICO O OBJETO
   objDetalhes=detalhes.data;
-  entrou();
   publicarMensagens();
-  setInterval(buscarMensagens,4500);
-}
-
-function entrou(){
-   corpo.innerHTML="";
-   corpo.innerHTML += `<div class="status mensagem"><h6>(${horas.toPrecision(2)}:${minutos.toPrecision(2)}:${segundos.toPrecision(2)})</h6><h1>${nome}</h1><h2>entrou na sala...</h2></div>`;
 }
 
 function publicarMensagens(){
+ corpo.innerHTML="";
    for(let i=0; i<objDetalhes.length; i++){
       
       if(objDetalhes[i].type=="status"){
         const  mensagemStatus=`<div class="status mensagem"><h6>(${objDetalhes[i].time})</h6><h1>${objDetalhes[i].from}</h1><h2>${objDetalhes[i].text}</h2></div>`
-        corpo.innerHTML += mensagemStatus;
-      } else if(objDetalhes[i].type=="private_message" && objDetalhes[i].to === nome){
+        corpo.innerHTML += mensagemStatus;}
+      if(objDetalhes[i].type=="private_message" && ( objDetalhes[i].to === nome ||  objDetalhes[i].from ===nome)){
          const mensagemPrivada= `<div class="chat-privado mensagem"><h6>(${objDetalhes[i].time})</h6><h1>${objDetalhes[i].from}</h1><h2>reservadamente para </h2><h1>${objDetalhes[i].to}</h1><h2>${objDetalhes[i].text}</h2></div>`
-         corpo.innerHTML += mensagemPrivada;      
-      }else{
+         corpo.innerHTML += mensagemPrivada;}     
+      if(objDetalhes[i].type=="message"){
         const mensagens= `<div class="chat-publico mensagem"><h6>(${objDetalhes[i].time})</h6><h1>${objDetalhes[i].from}</h1><h2>para</h2><h1>${objDetalhes[i].to}</h1><h2>${objDetalhes[i].text}</h2></div>`
         corpo.innerHTML += mensagens;
       }
    }  
-   const ultimaMensagem =document.querySelector(".mensagem:last-child");
-   ultimaMensagem.scrollIntoView();
+   //const ultimaMensagem =document.querySelector(".mensagem:last-child");
+   //ultimaMensagem.scrollIntoView();
 }
+let upmensagem;
+let nickName="Todos";
+let tipoMensagem="message";
 
 function novaMensagem(el){
    let novasMensagens = document.querySelector(".input-mensagem").value;
-   let upmensagem={
+      upmensagem={
       from: nome,
-	   to: "Todos",
+	   to: nickName,
 	   text: novasMensagens,
-   	type: "message"
+   	type: tipoMensagem
    }
 
    promise=axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",upmensagem);
    promise.then(buscarMensagens);
    promise.catch(alertar);
+   document.querySelector(".input-mensagem").value="";
 }
 
 function alertar(erro){
@@ -99,13 +86,11 @@ function alertar(erro){
 }
 
 /*************************************************  BOTOES BONUS **************************************************/
-let contatos;
-let nickName;
 
+setInterval(BuscarParticipantes,10000);
 function verParticipantes(el){
    document.querySelector(".participantes").classList.add("mostrar");
    document.querySelector(".divisao").classList.remove("esconder");
-   BuscarParticipantes();
 }
 
 function voltarChat(el){
@@ -127,29 +112,25 @@ let TodosContatos=document.querySelector(".todosContatos");
 function verUsuarios(){
    for(let i=0;i<contatos.length;i++){
       const listaContatos= `<div class="contatos"><ion-icon class="iconeBonus" name="person-circle-sharp"></ion-icon><h5 onclick="selecionarChat(this)"> ${contatos[i].name}</h5><ion-icon name="checkmark-sharp" class="ok esconder"></ion-icon> `
-     // console.log(contatos[i].name);
       TodosContatos.innerHTML += listaContatos;
    }
 }
+let privado=document.querySelector(".privado");
+let publico=document.querySelector(".publico");
 
 function selecionarChat(el){
    nickName = el.innerHTML;
    console.log(nickName);
-   //chatPrivado();
-
+  /* document.querySelectorAll(".contatos.ok").add("esconder");
+   el.querySelector.remove("esconder");*/
 }
-/*
-function chatPrivado(){
-   let novasMensagens = document.querySelector(".input-mensagem").value;
-   let upmensagem={   
-      from: nome,
-      to: nickName,
-      text: novasMensagens,
-      type: "private_message"
-   }
-   promise=axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",upmensagem);
-   promise.then(buscarMensagens);
-   promise.catch(alertar);
-}*/
-   
-
+function selecionarPrivacidade(){
+   tipoMensagem="private_message";
+   privado.classList.remove("esconder");
+   publico.classList.add("esconder");
+}
+function selecionarPublicidade(){
+   tipoMensagem="message";
+   privado.classList.add("esconder");
+   publico.classList.remove("esconder");
+}
